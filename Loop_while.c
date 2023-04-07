b21340eae5e28bea6a1f8e8c177dfe12fdd045b9
Ian <129607009+Borboletazul@users.noreply.github.com>
2023-04-07 16:12:03 -0300

    Add files via upload

---
+++ Loop_while.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+
+  // Guarda o numero escolhido
+  int numero_secreto = 27;
+  int achou = 0;
+
+  printf("Adivinhe o número secreto!\n");
+  printf("Você tem 10 tentativas...\n");
+  while (achou == 0) {
+    // ler o palpite do usuário
+    int palpite_jogador;
+    printf("\nQual seu palpite? :");
+    scanf("%d", &palpite_jogador);
+
+    // compare o valor do jogador com o valor do computador
+    if (palpite_jogador == numero_secreto) {
+      printf("Você acertou o número secreto!!!");
+      achou = 1;
+    }
+    else{
+      printf("Número incorreto!\n");
+    }
+  }
+  return 0;
+}
